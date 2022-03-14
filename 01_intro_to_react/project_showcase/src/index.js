33671f5327c8caeff3501616138d159ca3f90f4b
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-14 13:22:14 -0700

    fin lecture 3/14

--- 01_intro_to_react/project_showcase/src/index.js
+++ 01_intro_to_react/project_showcase/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//importing libraries
 
+import React from "react";
+import ReactDOM from "react-dom";
+//import './index.css';
+
+//importing App component from App.js
+import App from "./App";
+
+//first argument is the high level component rendered to the DOM. It will have the rest of the UI nested inside it.
+//the second argument is teh el that appends the html (root (a div) found in index.html)
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <>This is a react fragment. This is just surface level.</>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
