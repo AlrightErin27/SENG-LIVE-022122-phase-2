778bab9b4dd34a90cacd4cbc1138bc4fc1f85aa9
Erin Van Brunt <erinmontybruce@gmail.com>
2022-03-14 10:23:15 -0700

    init cvommit

---
+++ 01_intro_to_react/notes.jsx
@@ -0,0 +1,3 @@
+//Declarative Syntax---> vs. imperative (you tell the computer what to do step by step) & declarative is you telling
+//---------------------> the computer what you'd like the end result to be, and it does the step by step.
+//---------------------> react is declarative(JSX). vanilla JS w/o this framework is not.
